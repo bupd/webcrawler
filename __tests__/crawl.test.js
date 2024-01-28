58637adef190bca68704f15191d70c47455a89d2
bupd <bupdprasanth@gmail.com>
2024-01-29 04:59:56 +0530

    Add tests for normalizeUrls
    Signed-off-by: bupd <bupdprasanth@gmail.com>

---
+++ __tests__/crawl.test.js
@@ -0,0 +1,32 @@
+const { normalizeUrl } = require("../crawl.js");
+const { test, expect } = require("@jest/globals");
+
+test("normalizeUrl", () => {
+  const input = "https://bupd.github.io/";
+  const actual = normalizeUrl(input);
+  const expected = "bupd.github.io";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeUrl with query params", () => {
+  const input = "https://bupd.github.io";
+  const actual = normalizeUrl(input);
+  const expected = "bupd.github.io";
+  expect(actual).toEqual(expected);
+});
+
+
+test("normalizeUrl with query params", () => {
+  const input = "https://bupd.github.io/kumar/?q=hello";
+  const actual = normalizeUrl(input);
+  const expected = "bupd.github.io/kumar";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeUrl with query params", () => {
+  const input = "https://bupd.github.io/kumar/?q=hello";
+  const actual = normalizeUrl(input);
+  const expected = "bupd.github.io/kumar";
+  expect(actual).toEqual(expected);
+});
+
