3b49dfc58cf0e43cea063055633f439a16f63b78
bupd <bupdprasanth@gmail.com>
2024-01-29 04:43:52 +0530

    Added configs and inits
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("vanakkam kumaaru.")
+function isEven(number) {
+  if (number < 0) throw new Error("Number must be positive");
+  if (typeof number !== "number") throw new Error("Number must be a number");
+  return number % 2 === 0;
+}
+
+module.exports = isEven;
