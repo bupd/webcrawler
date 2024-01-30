cb4dcf39f34f818b9e993843644c909a722f45bd
bupd <bupdprasanth@gmail.com>
2024-01-30 12:35:29 +0530

    handle relative urls

--- crawl.js
+++ crawl.js
@@ -1,3 +1,34 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsfromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+
+  for (const linkElement of linkElements) {
+    
+    // Check if it's a relative url
+    if (linkElement.href.startsWith("/")) {
+      // relative url block
+      try {
+        const url = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(`${url.hostname}${url.pathname}/`);
+      } catch (err) {
+        console.error("error in relative url", err.message);
+      }
+    } else {
+      // normal url block
+      try {
+        const url = new URL(linkElement.href);
+        urls.push(`${url.hostname}${url.pathname}`);
+      } catch (err) {
+        console.error("error in relative url", err.message);
+      }
+    }
+
+  }
+  return urls;
+}
 function normalizeUrl(UrlString) {
   const urlObj = new URL(UrlString);
 
@@ -8,4 +39,4 @@ function normalizeUrl(UrlString) {
   return normalizedURL;
 }
 
-module.exports = { normalizeUrl };
+module.exports = { normalizeUrl, getURLsfromHTML };
