9d7eed7f73425d8c50e2a4124d18351b64767c5b
bupd <bupdprasanth@gmail.com>
2024-01-30 12:36:12 +0530

    Add tests for getting urls

---
+++ __tests__/crawl.getURLs.test.js
@@ -0,0 +1,38 @@
+const { getURLsfromHTML } = require("../crawl.js");
+const { test, expect } = require("@jest/globals");
+
+test("getURLs absolute", () => {
+  const inputHTML = `
+<html>
+<body>
+  <a href="https://bupd.github.io">
+    bupd
+  </a>
+</body>
+</html>
+`;
+  const inputBaseUrl = "https://bupd.github.io";
+  const actual = getURLsfromHTML(inputHTML, inputBaseUrl);
+  const expected = ["bupd.github.io/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLs relative", () => {
+  const inputHTML = `
+<html>
+<body>
+  <a href="https://bupd.github.io">
+    bupd
+  </a>
+  <a href="/relative/path">
+    bupd
+  </a>
+</body>
+</html>
+`;
+  const inputBaseUrl = "https://bupd.github.io";
+  const actual = getURLsfromHTML(inputHTML, inputBaseUrl);
+  const expected = ["bupd.github.io/", "bupd.github.io/relative/path/"];
+  expect(actual).toEqual(expected);
+});
+
