dadbf3f1e39fca506e2bfad3de3a4069117415c9
bupd <bupdprasanth@gmail.com>
2024-01-30 12:41:44 +0530

    tests: add invalid url

--- __tests__/crawl.getURLs.test.js
+++ __tests__/crawl.getURLs.test.js
@@ -36,3 +36,21 @@ test("getURLs relative", () => {
   expect(actual).toEqual(expected);
 });
 
+test("getURLs invalid", () => {
+  const inputHTML = `
+<html>
+<body>
+  <a href="https://bupd.github.io/">
+    bupd
+  </a>
+  <a href="invalid">
+    INVALID
+  </a>
+</body>
+</html>
+`;
+  const inputBaseUrl = "https://bupd.github.io";
+  const actual = getURLsfromHTML(inputHTML, inputBaseUrl);
+  const expected = ["bupd.github.io/"];
+  expect(actual).toEqual(expected);
+});
